799387888ee3f30b62521126bd888ece389f85c0
duyawen <duyawen1215@gmail.com>
2023-06-20 20:49:05 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
+name = 'Zoey'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-def hello():
-    return 'Welcom to CyberMaster!'
+# def hello():
+#     return 'Welcom to CyberMaster!'
 
 # @app.route('/user/<name>')
 # def user_page(name):
@@ -20,3 +34,9 @@ def hello():
 #     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
 #     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
 #     return 'Test page'
+
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
+
