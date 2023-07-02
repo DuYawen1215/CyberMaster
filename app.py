d69998839ac6a27210a9b45a5d9c01721b5c1ef2
duyawen <duyawen1215@gmail.com>
2023-07-02 22:45:46 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -63,6 +63,15 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()  #读取所有用户，并注入到每个模版中
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+@app.errorhandler(404)  # 传入要处理的代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+
 @app.route('/')
 # def hello():
 #     return 'Welcom to CyberMaster!'
@@ -80,11 +89,9 @@ def forge():
 #     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
 #     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
 #     return 'Test page'
-
 def index():
-    user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 
 
